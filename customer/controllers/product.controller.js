c34dba38b9e7ab316d3f91a6b61f8c22994db7ab
Abishake505 <abishake175@gmail.com>
2025-07-03 02:58:33 +0530

    fifth commit

--- customer/controllers/product.controller.js
+++ customer/controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/productModel.js');
 
 const getproducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
